5b95a4b0262809d9834abbd50f6645d3ad009f7b
BigCelz <stanleychilaka22270@gmail.com>
2026-01-04 00:43:50 +0100

    complete signup UI page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   try {
@@ -13,6 +14,6 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    next(error);
+    next(errorHandler(550, "Internal Server Error"));
   }
 };
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>
-      sign up page
+    <div className="p-4 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center my-7 font-semibold">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 border-gray-700 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          className="border border-gray-700 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border p-3 border-gray-700 rounded-lg"
+        />
+
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 cursor-pointer">
+          Sign Up
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-4">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-700 hover:underline">Sign In</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
