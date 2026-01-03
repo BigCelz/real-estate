c3f5debf548cdbc38fba21de43e56a56a1a90675
BigCelz <stanleychilaka22270@gmail.com>
2026-01-04 00:16:11 +0100

    signup API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -11,10 +13,15 @@ mongoose.connect(process.env.MONGO).then(()=> {
 });
 
 const app = express();
+app.use(express.json());
 
 app.get('/test', (req, res) => {
     res.send('API is working');
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
 
 app.listen(3000, ()=> {
     console.log('Server running on port 3000');
