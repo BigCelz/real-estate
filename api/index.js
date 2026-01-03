beadecd7a676533920e134d798afa10a207da42d
BigCelz <stanleychilaka22270@gmail.com>
2026-01-03 23:29:03 +0100

    creating user model, controller, test api route

--- api/index.js
+++ api/index.js
@@ -12,6 +12,10 @@ mongoose.connect(process.env.MONGO).then(()=> {
 
 const app = express();
 
+app.get('/test', (req, res) => {
+    res.send('API is working');
+});
+
 app.listen(3000, ()=> {
     console.log('Server running on port 3000');
-})
+});
