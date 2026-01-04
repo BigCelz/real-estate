762af60578b8655014dc91f1db1b7bf1fc70658a
BigCelz <stanleychilaka22270@gmail.com>
2026-01-04 01:38:39 +0100

    signup frontend functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,15 +5,25 @@ import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
+
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({
+        success: false, 
+        message: "User already exists",
+      });
     }
+
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+
+    res.status(201).json({
+      success: true,     
+      message: "User created successfully",
+    });
   } catch (error) {
     next(errorHandler(550, "Internal Server Error"));
   }
 };
+
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,32 +1,77 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [FormData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...FormData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(FormData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError("");
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError("Something went wrong. Please try again.");
+    }
+  };
+
   return (
-    <div className="p-4 max-w-lg mx-auto">
+    <div className="p-4 max-w-lg !w-full mx-auto !overflow-x-hidden">
       <h1 className="text-3xl text-center my-7 font-semibold">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           id="username"
-          className="border p-3 border-gray-700 rounded-lg"
+          className="border w-full p-3 border-gray-700 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="email"
           id="email"
-          className="border border-gray-700 p-3 rounded-lg"
+          className="border w-full border-gray-700 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           id="password"
-          className="border p-3 border-gray-700 rounded-lg"
+          className="border w-full p-3 border-gray-700 rounded-lg"
+          onChange={handleChange}
         />
 
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 cursor-pointer">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 w-full text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 cursor-pointer"
+        >
+          {loading ? "Signing Up..." : "Sign Up"}
         </button>
       </form>
 
@@ -35,7 +80,8 @@ export default function SignUp() {
         <Link to="/sign-in">
           <span className="text-blue-700 hover:underline">Sign In</span>
         </Link>
-      </div>
+      </div>.
+      {error && <p className="text-red-600 mt-1">{error}</p>}
     </div>
   );
 }
